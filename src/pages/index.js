57d1fb60faba68975bff49234cdb9b52ec147e26
Peter Mada <petermada@gmail.com>
2022-08-16 10:51:02 +0200

    Typewriter animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import * as React from 'react';
 
+import { Typewriter } from '../components/Typewriter/Typewriter';
+
 const IndexPage = () => {
   return (
     <main data-testid='indexPage'>
       <h1>Peter Mada</h1>
+      <Typewriter dynamicText='ABCDEFGHIJ' />
     </main>
   );
 };
