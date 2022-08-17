7dee954331eaaf45694796f492fe7748c8906702
Peter Mada <petermada@gmail.com>
2022-08-17 17:43:59 +0200

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,29 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import { Layout } from '../components/Layout/Layout';
+import { Dots } from '../components/Dots/Dots';
 
 const NotFoundPage = () => {
-  return <main data-testid='pageNotFound'></main>;
+  return (
+    <Layout>
+      <div
+        data-testid='indexPage'
+        className='indexWrap relative overflow-hidden'>
+        <div className='flex justify-center h-screen'>
+          <div className='mt-40 2xl:mt-60 text-center'>
+            <h1 className='name text-5xl md:text-7xl md:tracking-wider font-bold text-primary dark:text-white mb-3'>
+              40<span className='fallingLetter'>4</span>
+            </h1>
+            <p className='mt-10 dark:text-white text-base text-lg md:text-2xl'>
+              Page not found
+            </p>
+          </div>
+        </div>
+
+        <Dots />
+      </div>
+    </Layout>
+  );
 };
 
 export default NotFoundPage;
