ef9e3e3f741c5f5de577537741a470c8c4e49e5e
Peter Mada <petermada@gmail.com>
2022-08-19 21:47:43 +0200

    Right number of paths in dots

--- testsUnit/components/Dots/Dots.test.js
+++ testsUnit/components/Dots/Dots.test.js
@@ -1,4 +1,6 @@
 import '@testing-library/jest-dom';
+import TestRenderer from 'react-test-renderer';
+
 import { render, screen } from '@testing-library/react';
 import { Dots } from '../../../src/components/Dots/Dots';
 
@@ -11,5 +13,16 @@ describe('Dots', () => {
   it('render a svg', () => {
     render(<Dots />);
     expect(screen.getByTestId('svgDots').tagName).toEqual('svg');
+  });
+
+  // TODO snapshot test does not work becouse of randomnes in svg
+  it.skip('render svg correctly', () => {
+    const tree = TestRenderer.create(<Dots />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('has right number of paths in svg', () => {
+    const { container } = render(<Dots />);
+    expect(container.querySelectorAll('path').length).toBe(1562);
   });
 });
